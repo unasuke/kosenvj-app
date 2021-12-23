8f340841736610bb89adc43f2814217704680089
Yusuke Nakamura <yusuke1994525@gmail.com>
2021-12-24 02:47:24 +0900

    Use selector to handling midi device state

--- src/atoms/circuit.ts
+++ src/atoms/circuit.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 type Circuit = {
   knob1: number;
@@ -21,5 +21,80 @@ export const circuitState = atom<Circuit>({
     knob6: 0,
     knob7: 0,
     knob8: 0,
+  },
+});
+
+export const circuitKnob1State = selector<number>({
+  key: "circuitKnob1State",
+  get: ({ get }) => get(circuitState).knob1,
+  set: ({ get, set }, newValue) => {
+    if (typeof newValue === "number") {
+      set({ ...circuitState }, { ...get(circuitState), knob1: newValue });
+    }
+  },
+});
+
+export const circuitKnob2State = selector<number>({
+  key: "circuitKnob2State",
+  get: ({ get }) => get(circuitState).knob1,
+  set: ({ get, set }, newValue) => {
+    if (typeof newValue === "number") {
+      set({ ...circuitState }, { ...get(circuitState), knob2: newValue });
+    }
+  },
+});
+
+export const circuitKnob3State = selector<number>({
+  key: "circuitKnob3State",
+  get: ({ get }) => get(circuitState).knob1,
+  set: ({ get, set }, newValue) => {
+    if (typeof newValue === "number") {
+      set({ ...circuitState }, { ...get(circuitState), knob3: newValue });
+    }
+  },
+});
+export const circuitKnob4State = selector<number>({
+  key: "circuitKnob4State",
+  get: ({ get }) => get(circuitState).knob1,
+  set: ({ get, set }, newValue) => {
+    if (typeof newValue === "number") {
+      set({ ...circuitState }, { ...get(circuitState), knob4: newValue });
+    }
+  },
+});
+export const circuitKnob5State = selector<number>({
+  key: "circuitKnob5State",
+  get: ({ get }) => get(circuitState).knob1,
+  set: ({ get, set }, newValue) => {
+    if (typeof newValue === "number") {
+      set({ ...circuitState }, { ...get(circuitState), knob5: newValue });
+    }
+  },
+});
+export const circuitKnob6State = selector<number>({
+  key: "circuitKnob6State",
+  get: ({ get }) => get(circuitState).knob1,
+  set: ({ get, set }, newValue) => {
+    if (typeof newValue === "number") {
+      set({ ...circuitState }, { ...get(circuitState), knob6: newValue });
+    }
+  },
+});
+export const circuitKnob7State = selector<number>({
+  key: "circuitKnob7State",
+  get: ({ get }) => get(circuitState).knob1,
+  set: ({ get, set }, newValue) => {
+    if (typeof newValue === "number") {
+      set({ ...circuitState }, { ...get(circuitState), knob7: newValue });
+    }
+  },
+});
+export const circuitKnob8State = selector<number>({
+  key: "circuitKnob8State",
+  get: ({ get }) => get(circuitState).knob1,
+  set: ({ get, set }, newValue) => {
+    if (typeof newValue === "number") {
+      set({ ...circuitState }, { ...get(circuitState), knob8: newValue });
+    }
   },
 });
--- src/components/handleMidi.tsx
+++ src/components/handleMidi.tsx
@@ -1,7 +1,16 @@
 import { useEffect } from "react";
-import { WebMidi } from "webmidi";
+import { Input, WebMidi } from "webmidi";
 import { useRecoilState } from "recoil";
-import { circuitState } from "../atoms/circuit";
+import {
+  circuitKnob1State,
+  circuitKnob2State,
+  circuitKnob3State,
+  circuitKnob4State,
+  circuitKnob5State,
+  circuitKnob6State,
+  circuitKnob7State,
+  circuitKnob8State,
+} from "../atoms/circuit";
 
 const knob1Number = 80;
 const knob2Number = 81;
@@ -13,52 +22,60 @@ const knob7Number = 86;
 const knob8Number = 87;
 
 export function HandleMidi() {
-  const [circuit, setCircuit] = useRecoilState(circuitState);
+  // const [circuit, setCircuit] = useRecoilState(circuitState);
+  const [knob1, setKnob1] = useRecoilState(circuitKnob1State);
+  const [knob2, setKnob2] = useRecoilState(circuitKnob2State);
+  const [knob3, setKnob3] = useRecoilState(circuitKnob3State);
+  const [knob4, setKnob4] = useRecoilState(circuitKnob4State);
+  const [knob5, setKnob5] = useRecoilState(circuitKnob5State);
+  const [knob6, setKnob6] = useRecoilState(circuitKnob6State);
+  const [knob7, setKnob7] = useRecoilState(circuitKnob7State);
+  const [knob8, setKnob8] = useRecoilState(circuitKnob8State);
+
+  let device: Input;
 
   useEffect(() => {
     WebMidi.enable().then(() => {
       console.log("WebMidi Enabled");
-      const device = WebMidi.getInputByName("Circuit");
+      device = WebMidi.getInputByName("Circuit");
       if (device) {
         device.addListener("midimessage", (event) => {
-          if (Math.random() > 0.3 && event.message.type !== "clock") {
-            console.log(`handleMidi: ${event.data}`);
-          }
+          // if (Math.random() > 0.3 && event.message.type !== "clock") {
+          //   console.log(`handleMidi: ${event.data}`);
+          // }
           switch (event.data[1]) {
             case knob1Number:
-              setCircuit({ ...circuit, knob1: event.data[2] });
+              setKnob1(event.data[2]);
               break;
             case knob2Number:
-              setCircuit({ ...circuit, knob2: event.data[2] });
+              setKnob2(event.data[2]);
               break;
             case knob3Number:
-              setCircuit({ ...circuit, knob3: event.data[2] });
+              setKnob3(event.data[2]);
               break;
             case knob4Number:
-              setCircuit({ ...circuit, knob4: event.data[2] });
+              setKnob4(event.data[2]);
               break;
             case knob5Number:
-              setCircuit({ ...circuit, knob5: event.data[2] });
+              setKnob5(event.data[2]);
               break;
             case knob6Number:
-              setCircuit({ ...circuit, knob6: event.data[2] });
+              setKnob6(event.data[2]);
               break;
             case knob7Number:
-              setCircuit({ ...circuit, knob7: event.data[2] });
+              setKnob7(event.data[2]);
               break;
             case knob8Number:
-              setCircuit({ ...circuit, knob8: event.data[2] });
+              setKnob8(event.data[2]);
               break;
           }
         });
       }
     });
-  }, []);
-
-  useEffect(() => {
-    // setTimeout(() => {
-    //     setCircuit({...circuit, knob1: 1})
-    // }, 5000);
+    return () => {
+      device?.removeListener("midimessage");
+      console.info("midimessage listener removed");
+    };
   }, []);
 
   return (
